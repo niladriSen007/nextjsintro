4aee87b52be2e6b6d23de278be272bf5fde3af68
Niladri <niladrisengit12@gmail.com>
2023-06-12 11:41:06 +0530

    day-03 commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export const metadata = {
+  title: "Lama Dev Contact Information",
+  description: "This is Contact Page",
+};
+
 const Contact = () => {
   return (
-    <div className={styles.contact_container}>Contact</div>
-  )
-}
+    <div className={styles.contact_container}>
+      <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/rc.jpg"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="20"
+            rows="6"
+          ></textarea>
+          <button className="button">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
